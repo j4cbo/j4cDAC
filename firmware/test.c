ced55ec55661121054fa2757bd70e487687309b4
Jacob Potter <jacob@durbatuluk.us>
2010-12-10 07:14:47 -0500

    Blinkenlichten.

--- firmware/test.c
+++ firmware/test.c
@@ -1,5 +1,25 @@
 #include "LPC17xx.h"
 
+volatile uint32_t time;
+
+void SysTick_Handler(void) {
+	time++;
+}
+
+void delay_ms(uint16_t length) {
+	uint32_t end = time + length;
+	while (time < end);
+}
+
 int main() {
-	while(1);
+	SysTick_Config(SystemCoreClock / 1000);
+
+	LPC_GPIO0->FIODIR = 1;
+
+	while(1) {
+		LPC_GPIO0->FIOPIN = 1;
+		delay_ms(500);
+		LPC_GPIO0->FIOPIN = 0;
+		delay_ms(500);
+	}
 }
