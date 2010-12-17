c9f0a52b2a7528d3f7fe78eb835fca40ea3f32ac
Jacob Potter <jacob@durbatuluk.us>
2010-12-16 21:56:05 -0500

    Drop PHY timeout a bit. This makes the system boot much more quickly when there is no Ethernet cable...

--- firmware/inc/ether_private.h
+++ firmware/inc/ether_private.h
@@ -346,7 +346,7 @@ EMAC_RINFO_LEN_ERR   | EMAC_RINFO_ALIGN_ERR | EMAC_RINFO_OVERRUN)
 /* DP83848C PHY definition ------------------------------------------------------------ */
 
 /** PHY device reset time out definition */
-#define EMAC_PHY_RESP_TOUT		0x100000UL
+#define EMAC_PHY_RESP_TOUT		0x20000UL
 
 /* ENET Device Revision ID */
 #define EMAC_OLD_EMAC_MODULE_ID  0x39022000  /**< Rev. ID for first rev '-'         */
