0d6adc7430d14e0e39507688c213f1edea18fe42
Jacob Potter <jacob@durbatuluk.us>
2010-12-30 00:11:55 -0500

    GPLv3

--- firmware/inc/dac.h
+++ firmware/inc/dac.h
@@ -1,3 +1,20 @@
+/* j4cDAC DMA/DAC driver
+ *
+ * Copyright 2010 Jacob Potter
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef DAC_H
 #define DAC_H
 
