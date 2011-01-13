d4eb60dd676c70453f6d7bb1ec60c30061ebc258
Jacob Potter <jacob@durbatuluk.us>
2011-01-12 21:51:12 -0500

    Draw something more interesting.

--- tools/tester/talk.py
+++ tools/tester/talk.py
@@ -18,6 +18,27 @@
 
 import dac
 
+class SquarePointStream(object):
+	def produce(self):
+		pmax = 32600
+		pstep = 200
+		cmax = 65535
+		while True:
+			for x in xrange(-pmax, pmax, pstep):
+				yield (x, pmax, cmax, 0, 0)
+			for y in xrange(pmax, -pmax, -pstep):
+				yield (pmax, y, 0, cmax, 0)
+			for x in xrange(pmax, -pmax, -pstep):
+				yield (x, -pmax, 0, 0, cmax)
+			for y in xrange(-pmax, pmax, pstep):
+				yield (-pmax, y, cmax, cmax, cmax)
+
+	def __init__(self):
+		self.stream = self.produce()
+
+	def read(self, n):
+		return [self.stream.next() for i in xrange(n)]
+
 class NullPointStream(object):
 	def read(self, n):
 		return [(0, 0, 0, 0, 0)] * n
@@ -26,4 +47,4 @@ class NullPointStream(object):
 
 d = dac.DAC("169.254.13.29")
 
-d.play_stream(NullPointStream())
+d.play_stream(SquarePointStream())
