8a6642a7be52546f9c3ec766a645b0f1c0c4781c
Jacob Potter <jacob@durbatuluk.us>
2011-03-10 20:30:32 -0500

    Shrink LWIP's TCP window. This prevents dropped packets due to overflow.

--- firmware/inc/lwipopts.h
+++ firmware/inc/lwipopts.h
@@ -41,7 +41,7 @@
 #define MEMP_NUM_TCP_PCB_LISTEN	3
 
 #define TCP_MSS         1460
-#define TCP_WND		10000
+#define TCP_WND		7500
 #define TCP_SND_BUF     (2 * TCP_MSS)
 #define TCP_SND_QUEUELEN 8
 
