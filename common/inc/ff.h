527847e5fc0da719aea9c524340afc71092f230d
Jacob Potter <jacob@durbatuluk.us>
2012-01-20 23:57:07 -0500

    Merge integer.h into ff.h

--- common/inc/ff.h
+++ common/inc/ff.h
@@ -21,7 +21,25 @@
 extern "C" {
 #endif
 
-#include "integer.h"	/* Basic integer types */
+/* These types must be 16-bit, 32-bit or larger integer */
+typedef int				INT;
+typedef unsigned int	UINT;
+
+/* These types must be 8-bit integer */
+typedef unsigned char	UCHAR;
+typedef unsigned char	BYTE;
+
+/* These types must be 16-bit integer */
+typedef short			SHORT;
+typedef unsigned short	USHORT;
+typedef unsigned short	WORD;
+typedef unsigned short	WCHAR;
+
+/* These types must be 32-bit integer */
+typedef long			LONG;
+typedef unsigned long	ULONG;
+typedef unsigned long	DWORD;
+
 #include "ffconf.h"		/* FatFs configuration options */
 
 #if _FATFS != _FFCONF
