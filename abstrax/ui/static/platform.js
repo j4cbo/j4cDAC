c814b05e95af382159e65583168b8bdce23336eb
Andrew Drake <adrake@adrake.org>
2012-11-23 18:29:37 -0800

    Add suport for Microsoft pointer APIs and WinRT socket APIs.

---
+++ abstrax/ui/static/platform.js
@@ -0,0 +1,10 @@
+﻿// Platform detection
+var pointerApi = "portable";
+if (window.navigator.msPointerEnabled) {
+    pointerApi = "mspointer";
+}
+
+var socketApi = "socket.io";
+if ("Windows" in window) {
+    socketApi = "winrt";
+}
