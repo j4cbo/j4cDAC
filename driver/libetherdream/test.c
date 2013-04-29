478e9cfbee8f22bc6a703ec4a169d564b91e64d0
Jacob Potter <jacob@durbatuluk.us>
2013-04-28 19:56:39 -0700

    libetherdream test

--- driver/libetherdream/test.c
+++ driver/libetherdream/test.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "etherdream.h"
 
@@ -21,20 +22,79 @@ uint16_t colorsin(float pos) {
 	return res;
 }
 
-void fill_circle(float phase) {
+void fill_circle(float phase, int mode) {
 	int i;
 	for (i = 0; i < CIRCLE_POINTS; i++) {
 		struct etherdream_point *pt = &circle[i];
 		float ip = (float)i * 2.0 * M_PI / (float)CIRCLE_POINTS;
-		pt->x = sin(ip) * 20000; 
-		pt->y = cos(ip) * 20000; 
-		pt->r = colorsin(ip + phase);
-		pt->g = colorsin(ip + (2.0 * M_PI / 3.0) + phase);
-		pt->b = colorsin(ip + (4.0 * M_PI / 3.0) + phase);
+		float ipf = fmod(ip + phase, 2.0 * M_PI);;
+
+		switch (mode) {
+		default:
+		case 0: {
+			float cmult = .05 * sin(30 * (ip - phase/3));
+			pt->x = sin(ip) * 20000 * (1 + cmult);
+			pt->y = cos(ip) * 20000 * (1 + cmult);
+			break;
+		}
+		case 1: {
+			float cmult = .10 * sin(10 * (ip - phase/3));
+			pt->x = sin(ip) * 20000 * (1 + cmult);
+			pt->y = cos(ip) * 20000 * (1 + cmult);
+			break;
+			/* XXX broken */
+/*
+			float R = 3;
+			float r = 5;
+			pt->x = 2000 * ((R-r)*cos(ip+phase) + r*cos((R-r)*ip/r));
+			pt->y = 2000 * ((R-r)*sin(ip+phase) - r*sin((R-r)*ip/r));
+			break;
+*/
+		}
+		case 2: {
+			ip *= 3;
+			float R = 5;
+			float r = 3;
+			float D = 5;
+
+			pt->x = 2500 * ((R-r)*cos(ip + phase) + D*cos((R-r)*ip/r));
+			pt->y = 2500 * ((R-r)*sin(ip + phase) - D*sin((R-r)*ip/r));
+			break;
+		}
+		case 3: {
+			int n = 5;
+			float R = 5 * cos(M_PI/n) / cos(fmod(ip, (2*M_PI/n)) - (M_PI/n));
+			pt->x = 3500 * R*cos(ip+phase);
+			pt->y = 3500 * R*sin(ip+phase);
+			break;
+		}
+		case 4: {
+			float Xo = sin(ip);
+			pt->x = 20000 * Xo * cos(phase/4);
+			pt->y = 20000 * Xo * -sin(phase/4);
+			ipf = fmod(((Xo + 1) / 2.0) + phase / 3, 1.0) * 2 * M_PI;
+		}
+		}
+
+		pt->r = colorsin(ipf);
+		pt->g = colorsin(ipf + (2.0 * M_PI / 3.0));
+		pt->b = colorsin(ipf + (4.0 * M_PI / 3.0));
+/*
+		if (ipf < 2.0 * M_PI / 3.0) {
+			pt->r = 65535;
+			pt->g = pt->b = 0;
+		} else if (ipf < 4.0 * M_PI / 3.0) {
+			pt->g = 65535;
+			pt->r = pt->b = 0;
+		} else {
+			pt->b = 65535;
+			pt->r = pt->g = 0;
+		}
+*/
 	}
 }
 
-int main() {
+int main(int argc, char **argv) {
 	etherdream_lib_start();
 
 	/* Sleep for a bit over a second, to ensure that we see broadcasts
@@ -46,6 +106,12 @@ int main() {
 		printf("No DACs found.\n");
 		return 0;
 	}
+
+	int mode;
+	if (argc > 1)
+		mode = atoi(argv[1]);
+	else
+		mode = 0;
 
 	int i;
 	for (i = 0; i < cc; i++) {
@@ -61,7 +127,7 @@ int main() {
 
 	i = 0;
 	while (1) {
-		fill_circle((float)i / 50);
+		fill_circle((float)i / 50, mode);
 		int res = etherdream_write(d, circle, CIRCLE_POINTS, 30000, 1);
 		if (res != 0) {
 			printf("write %d\n", res);
