352464bfb4d2d153e92d2e5bccd204bf7316bc3a
Jacob Potter <jacob@durbatuluk.us>
2015-08-17 00:10:57 -0400

    Convert libetherdream etherdream.c to be polyglot C-intersect-C++

--- driver/libetherdream/etherdream.c
+++ driver/libetherdream/etherdream.c
@@ -139,8 +139,10 @@ static long long microseconds(void) {
  * Like usleep().
  */
 static void microsleep(long long us) {
-	nanosleep(&(struct timespec){ .tv_sec = us / 1000000,
-	                             .tv_nsec = (us % 1000000) * 1000 }, NULL);
+	struct timespec t;
+	t.tv_sec = us / 1000000;
+	t.tv_nsec = (us % 1000000) * 1000;
+	nanosleep(&t, NULL);
 }
 
 /* trace(d, fmt, ...)
@@ -190,9 +192,11 @@ static int wait_for_fd_activity(struct etherdream *d, int usec, int writable) {
 	fd_set set;
 	FD_ZERO(&set);
 	FD_SET(d->conn.dc_sock, &set);
+	struct timeval t;
+	t.tv_sec = usec / 1000000;
+	t.tv_usec = usec % 1000000;
 	int res = select(d->conn.dc_sock + 1, (writable ? NULL : &set),
-		(writable ? &set : NULL), &set, &(struct timeval){
-		.tv_sec = usec / 1000000, .tv_usec = usec % 1000000 });
+		(writable ? &set : NULL), &set, &t);
 	if (res < 0)
 		log_socket_error(d, "select");
 
@@ -328,52 +332,60 @@ static int dac_connect(struct etherdream *d) {
 	}
 
 	// Wait for connection to go through
-	int res = wait_for_fd_activity(d, DEFAULT_TIMEOUT, 1);
-	if (res < 0)
-		goto bail;
-	if (res == 0) {
-		trace(d, "Connection to %s timed out.\n", inet_ntoa(d->addr));
-		goto bail;
+	{
+		int res = wait_for_fd_activity(d, DEFAULT_TIMEOUT, 1);
+		if (res < 0)
+			goto bail;
+		if (res == 0) {
+			trace(d, "Connection to %s timed out.\n", inet_ntoa(d->addr));
+			goto bail;
+		}
 	}
 
 	// See if we have *actually* connected
-	int error;
-	unsigned int len = sizeof error;
-	if (getsockopt(conn->dc_sock, SOL_SOCKET, SO_ERROR, (char *)&error,
-	                                                           &len) < 0) {
-		log_socket_error(d, "getsockopt");
-		goto bail;
+	{
+		int error;
+		unsigned int len = sizeof error;
+		if (getsockopt(conn->dc_sock, SOL_SOCKET, SO_ERROR, (char *)&error,
+		                                                           &len) < 0) {
+			log_socket_error(d, "getsockopt");
+			goto bail;
+		}
+
+		if (error) {
+			errno = error;
+			log_socket_error(d, "connect");
+			goto bail;
+		}
 	}
 
-	if (error) {
-		errno = error;
-		log_socket_error(d, "connect");
-		goto bail;
-	}
-
-	int ndelay = 1;
-	if (setsockopt(conn->dc_sock, IPPROTO_TCP, TCP_NODELAY,
-	                                (char *)&ndelay, sizeof(ndelay)) < 0) {
-		log_socket_error(d, "setsockopt TCP_NODELAY");
-		goto bail;
+	{
+		int ndelay = 1;
+		if (setsockopt(conn->dc_sock, IPPROTO_TCP, TCP_NODELAY,
+		                                (char *)&ndelay, sizeof(ndelay)) < 0) {
+			log_socket_error(d, "setsockopt TCP_NODELAY");
+			goto bail;
+		}
 	}
 
 	// After we connect, the DAC will send an initial status response
 	if (read_resp(d) < 0)
 		goto bail;
 
-	char c = 'p';
-	send_all(d, &c, 1);
+    {
+	    char c = 'p';
+	    send_all(d, &c, 1);
+    }
 
 	if (read_resp(d) < 0)
 		goto bail;
 	dump_resp(d);
 
 	if (d->sw_revision >= 2) {
-		c = 'v';
+		char c = 'v';
 		if (send_all(d, &c, 1) < 0)
 			goto bail;
-		res = read_bytes(d, d->version, sizeof(d->version));
+		int res = read_bytes(d, d->version, sizeof(d->version));
 		if (res < 0)
 			return res;
 	} else {
@@ -466,7 +478,7 @@ static int dac_send_data(struct etherdream *d, struct dac_point *data,
 	    && !d->conn.dc_begin_sent) {
 		trace(d, "L: Sending begin command...\n");
 
-		struct begin_command b = { .command = 'b', .point_rate = rate,
+		struct begin_command b = { .command = 'b', .point_rate = (uint32_t)rate,
 		                           .low_water_mark = 0 };
 		if ((res = send_all(d, (const char *)&b, sizeof b)) < 0)
 			return res;
@@ -844,7 +856,7 @@ static void *watch_for_dacs(void *arg) {
 
 		/* Make a new DAC entry */
 		struct etherdream *new_dac;
-		new_dac = (void *)malloc(sizeof (struct etherdream));
+		new_dac = (struct etherdream *)malloc(sizeof (struct etherdream));
 		if (!new_dac) {
 			trace(NULL, "!! malloc(struct etherdream) failed\n");
 			continue;
